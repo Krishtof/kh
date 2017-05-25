cd92c65f14795ac63ad42b7ac6bac0e1a39532f9
Krishtof <kristof.vizy@gmail.com>
2017-05-25 16:52:58 +0200

    index

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 	session_start();
 	error_reporting(0);
 	$_SESSION["loan_amount"] = number_format(800000,0,',',' ');
 	$_SESSION["loan_instalments"] = number_format(38046,0,',',' ');
 	$_SESSION["repaid_in"] = 24;
-	
+
 ?>
 <!DOCTYPE html>
 <html lang="en" class="no-js">
@@ -52,8 +52,22 @@
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
 	</p>
 
-<div class="container">
+<div class="container qualify">
 	<div class="col-md-6">
+		<h3>Do I qualify?</h3>
+		<ul>
+			<li>Are you older than 18 years?</li>
+			<li>Do you have access to your current account internet banking with one of the banks below?</li>
+			<li>Do you have sufficient income from a regular employment (apart from probation and notice period), pension or parental befnefits?</li>
+		</ul>
+
+		<h3>What should you have ready?</h3>
+		<ul>
+			<li>Your Valid ID</li>
+			<li>Your cell phone</li>
+			<li>Do you have sufficient income from a regular employment (apart from probation and notice period), pension or parental befnefits?</li>
+		</ul>
+
 
 	</div>
 	<div class="col-md-6 calculator">
@@ -92,6 +106,78 @@
 		<button onclick="window.location.href='basic.php'" class="bluebtn" type="button" name="button">Yes, I want this loan</button>
 
 	</div>
+	<div class="example">
+		<h3>Representative Example</h3>
+		<p>
+			For a 48-month, HUF 800 thousand loan, the monthly repayment instalment with regular monthly salary income is HUF 21,063 at a fixed 11.99% annual interest rate. The total amount to be repaid is HUF 1,033,536 and the loan charges are HUF 233,536, of which fees and costs amount to HUF 22,512. The annual percentage rate (APR) is 14.0%.
+		</p>
+	</div>
+	<h3>How does it work?</h3>
+	<div class="col-md-3">
+		<img src="img/peronal_data.png" />
+		<p>
+			Enter your personal Data
+		</p>
+	</div>
+
+
+	<div class="col-md-3">
+		<img src="img/take_pic.png" />
+		<p>
+			Take a picture of your ID card
+		</p>
+	</div>
+
+	<div class="col-md-3">
+		<img src="img/read_sign.png" />
+		<p>
+			Read and sign the contract online
+		</p>
+	</div>
+
+
+	<div class="col-md-3">
+		<img src="img/receive_enjoy.png" />
+		<p>
+			Receive and enjoy your money
+		</p>
+	</div>
+
+
+	<h3>What are the benefits?</h3>
+	<div class="col-md-3">
+		<img src="img/minutes.png" />
+		<p>
+			Money credited within 15 minutes after online contract signature.
+		</p>
+	</div>
+
+
+	<div class="col-md-3">
+		<img src="img/online_application.png" />
+		<p>
+			Purely online application without having to go to a branch.
+		</p>
+	</div>
+
+	<div class="col-md-3">
+		<img src="img/management.png" />
+		<p>
+			No management or other hidden fees.
+		</p>
+	</div>
+
+
+	<div class="col-md-3">
+		<img src="img/security.png" />
+		<p>
+			Reliability and tradition of our bank.
+		</p>
+	</div>
+
+
+
+
 </div>
 </div>
 
@@ -117,7 +203,7 @@
   		function changeRange(){
   			$('input[type="range"][name="amount"]').change(function(){
   				var value = $(this).val();
-  				
+
   				var css = getCssPercentage(300000,3000000,value);
   				$('input[type=range][name="amount"]').css('background','-webkit-linear-gradient(left, #00aeef 0%,#00aeef '+css+'%,#e6f7fe '+css+'%,#e6f7fe 100%)');
   				$('input[type=range][name="amount"]').css('background','-moz-linear-gradient(left, #00aeef 0%,#00aeef '+css+'%,#e6f7fe '+css+'%,#e6f7fe 100%)');
@@ -128,7 +214,7 @@
 				    data: {
 				    	mode: 'amountChange',
 				    	value: value
-				    }, 
+				    },
 				    success: function(msg){
 				    	var obj = $.parseJSON(msg);
 				    	$('input[type=range][name="monthly"]').attr('max',obj['loan_instalments'].replace(' ', ''));
@@ -150,31 +236,31 @@
 						//monthly_discount módosítása
 						$('#monthly_discount').html(obj['monthly_discount']+' Ft');
 						$('#total_saving').html(obj['total_saving']);
-						
+
 						$('#month_number').html(24);
 				    }
 				});
   			});
-  			
+
   		}
-  		
+
   		function getCssPercentage(min,max,value){
 	  		var percentage_1 = (max-min)/100;
 	  		var result = (value-min)/percentage_1;
 	  		return result;
   		}
-  		
+
   		function getCssPercentage2(min,max,value){
 	  		var percentage_1 = (max-min)/100;
 	  		var result = (value-min)/percentage_1;
 	  		return result;
   		}
-  		
+
   		function changeRange2(){
   			$('input[type="range"][name="monthly"]').change(function(){
   				var value = $(this).val();
   				var amount = $('input[type="range"][name="amount"]').val();
-  				
+
   				$.ajax({
 				    type: "POST",
 				    url: "ajax.php",
@@ -182,12 +268,12 @@
 				    	mode: 'monhtlyChange',
 				    	monthly: value,
 				    	amount : amount
-				    }, 
+				    },
 				    success: function(msg){
 				    	var obj = $.parseJSON(msg);
 				    	var max_monthly = $('input[type=range][name="monthly"]').attr('max');
 				    	var min_monthly = $('input[type=range][name="monthly"]').attr('min');
-				    	
+
 				    	//console.log(obj);
 				    	//input val módosítás
 				    	$('input[type="range"][name="monthly"]').val(obj['loan_instalments'].replace(' ', ''));
@@ -198,17 +284,17 @@
 						$('#total_saving').html(obj['total_saving']);
 						//css
 						var css = getCssPercentage2(min_monthly, max_monthly,obj['loan_instalments'].replace(' ', ''));
-						
+
 						$('input[type=range][name="monthly"]').css('background','-webkit-linear-gradient(left, #00aeef 0%,#00aeef '+css+'%,#e6f7fe '+css+'%,#e6f7fe 100%)');
 						$('input[type=range][name="monthly"]').css('background','-moz-linear-gradient(left, #00aeef 0%,#00aeef '+css+'%,#e6f7fe '+css+'%,#e6f7fe 100%)');
 						$('input[type=range][name="monthly"]').css('background','linear-gradient(left, #00aeef 0%,#00aeef '+css+'%,#e6f7fe '+css+'%,#e6f7fe 100%)');
-						
+
 						//month edit
 						$('#month_number').html(obj['repaid_in']);
 				    }
 				});
   			});
-  			
+
   		}
-  		
+
   	</script>
