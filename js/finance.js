24c27a6635c310c87da61a6990d1cd98f9708515
Tódor Márk <emaerka@gmail.com>
2017-05-31 01:03:57 +0200

    DropDown Fix

--- js/finance.js
+++ js/finance.js
@@ -4,12 +4,19 @@ $(function(){
 	bank_account();
 	jump_next();
 	dropdown();
+	a_prevent();
 });
 
 function dropdown(){
 	$('.dropdown-menu li').click(function(){
 		var selected = $(this).find('a').html();
-		$(this).closest('.dropdown').find('button.dropdown-toggle').html(selected);
+		$(this).closest('.dropdown').find('button.dropdown-toggle').html(selected+'<span class="caret"></span>');
+	});
+}
+
+function a_prevent(){
+	$('ul.dropdown-menu li a').click(function(e){
+		e.preventDefault();
 	});
 }
 
