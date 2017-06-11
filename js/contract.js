11f3c24478ab019470af4b3baccfc33bd3a6cf2e
Tódor Márk <emaerka@gmail.com>
2017-06-11 16:31:45 +0200

    Contract

--- js/contract.js
+++ js/contract.js
@@ -8,7 +8,10 @@ function next(){
 	$('#preco button').click(function(){
 		$('#precontractual').removeClass('in');
 		$('#precontractual_button').addClass('collapsed done');
-
+		
+		$('#precontractual .form').hide();
+		$('#precontractual .form.done').show();
+		
 		$('#terms').addClass('in');
 		$('#terms_button').removeClass('collapsed');
 		$('#terms_button').attr('data-target','#terms');
@@ -17,6 +20,10 @@ function next(){
 	$('#general button').click(function(){
 		$('#terms').removeClass('in');
 		$('#terms_button').addClass('collapsed done');
+		
+		$('#terms .form').hide();
+		$('#terms .form.done').show();
+		
 
 		$('#pricelist').addClass('in');
 		$('#pricelist_button').removeClass('collapsed');
@@ -26,6 +33,10 @@ function next(){
 	$('#list button').click(function(){
 		$('#pricelist').removeClass('in');
 		$('#pricelist_button').addClass('collapsed done');
+		
+		$('#pricelist .form').hide();
+		$('#pricelist .form.done').show();
+		
 
 		$('#contract').addClass('in');
 		$('#contract_button').removeClass('collapsed');
@@ -35,7 +46,10 @@ function next(){
 	$('#contr button').click(function(){
 		$('#contract').removeClass('in');
 		$('#contract_button').addClass('collapsed done');
-
+		
+		$('#contract .form').hide();
+		$('#contract .form.done').show();
+		
 		$('#bank').addClass('in');
 		$('#bank_button').removeClass('collapsed');
 		$('#bank_button').attr('data-target','#bank');
